b11c886fc5570740cbb13e0644dc54d4cab8ad26
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2024-04-09 12:21:41 +0200

    Fix ATable Clone method

--- 3DNineMensMorris/Assets/Scripts/Game scripts/Tables/ATable.cs
+++ 3DNineMensMorris/Assets/Scripts/Game scripts/Tables/ATable.cs
@@ -15,8 +15,19 @@ public class ATable : ICloneable
     {
         ATable clonedTable = new ATable(Board.GetLength(1), Board.GetLength(2), Board.GetLength(3));
 
-        Array.Copy(Board, clonedTable.Board, Board.Length);
-
+        for (int w = 0; w < Board.GetLength(0); w++)
+        {
+            for (int x = 0; x < Board.GetLength(1); x++)
+            {
+                for (int y = 0; y < Board.GetLength(2); y++)
+                {
+                    for (int z = 0; z < Board.GetLength(3); z++)
+                    {
+                        clonedTable.Board[w, x, y, z] = Board[w, x, y, z];
+                    }
+                }
+            }
+        }
         return clonedTable;
     }
 }
